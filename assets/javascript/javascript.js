c860b744f212360fa484f3e5538e2a1b24916223
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-15 17:17:29 +0000

    Added JavaScript for Group A

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,11 @@
+let players = ["Dave", "Bob", "Sue", "George", "Peter"]
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
+let p5 = document.getElementById('p5');
+let arr = [p1, p2, p3, p4, p5];
+
+for(let i = 0; i < players.length; i++){
+    arr[i].innerHTML = players[i];
+}
