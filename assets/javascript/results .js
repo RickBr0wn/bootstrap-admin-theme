4e5ceb800ef022252b953e32bf497bfcd2e806cd
RiCK BROWN <ricky.brown.00@gmail.com>
2018-02-25 23:57:11 +0000

    Struggling with result logic

--- assets/javascript/results .js
+++ assets/javascript/results .js
@@ -14,22 +14,34 @@ $(document).ready(function(){
         let matchG = createMatch(arr, 31);
         let matchH = createMatch(arr, 36);
 
+        let resultA = "Match not played.";
+        let resultB = "Match not played.";
+        let resultC = "Match not played.";
+        let resultD = "Match not played.";
+        let resultE = "Match not played.";
+        let resultF = "Match not played.";
+        let resultG = "Match not played.";
+        let resultH = "Match not played.";
+
         document.getElementById('matchA').innerHTML = matchA;
         document.getElementById('matchB').innerHTML = matchB;
         document.getElementById('matchC').innerHTML = matchC;
         document.getElementById('matchD').innerHTML = matchD;
         document.getElementById('matchE').innerHTML = matchE;
-        document.getElementById('tableF').innerHTML = matchF;
-        document.getElementById('tableG').innerHTML = matchG;
+        document.getElementById('matchF').innerHTML = matchF;
+        document.getElementById('matchG').innerHTML = matchG;
         document.getElementById('matchH').innerHTML = matchH;
+
     });
 });
 
 function createMatch(arr, group){
     console.log(arr);
     let grp = group-1;
-    let data = "<table><tr><td>";
+    let data = "<table><tr><th>Match</th><th>Results</th></tr><tr><td>";
     data += arr[0+grp].name + " vs " + arr[1+grp].name;
+    data += "</td><td>";
+    data += arr[0+grp].results[0]"</td>"
     data += "</td></tr><tr><td>";
     data += arr[2+grp].name + " vs " + arr[3+grp].name;
     data += "</td></tr><tr><td>";
